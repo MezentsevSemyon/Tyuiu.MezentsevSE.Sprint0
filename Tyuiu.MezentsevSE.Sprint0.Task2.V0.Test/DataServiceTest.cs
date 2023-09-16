6eaa46862bac6ab7ecb878da4480586515822cc1
Tyuiu.MezentsevSE@outlook.com <Tyuiu.MezentsevSE@outlook.com>
2023-09-16 20:03:32 +0500

    Task2

---
+++ Tyuiu.MezentsevSE.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MezentsevSE.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.MezentsevSE.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Семён";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет...,Семён", res);
+        }
+    }
+}
